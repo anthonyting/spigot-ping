bc4154c7b52c1689b35b848b56af1ddf18244eb2
Anthony Ting <anthonydting@gmail.com>
2020-03-22 18:12:32 -0700

    additional events that need a ping to update

--- src/main/java/com/xdefcon/spigotping/listeners/PingListener.java
+++ src/main/java/com/xdefcon/spigotping/listeners/PingListener.java
@@ -6,7 +6,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PingListener implements Listener {
@@ -20,6 +22,16 @@ public class PingListener implements Listener {
         SpigotPing plugin = SpigotPing.getInstance();
         plugin.getServer().getScheduler().runTaskLater(plugin,
                 () -> PingTabList.updateTabPing(event.getPlayer(), plugin), 20L);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerDeath(final PlayerDeathEvent event) {
+        PingTabList.updateTabPing(event.getEntity(), SpigotPing.getInstance());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onWorldChange(final PlayerChangedWorldEvent event) {
+        PingTabList.updateTabPing(event.getPlayer(), SpigotPing.getInstance());
     }
 
 }
